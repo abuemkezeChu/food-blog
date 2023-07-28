e71ede41f0a157d1dc28fd47e6552b98e71c4e49
Abuemkeze Chu <abuemkeze@gmail.com>
2023-07-28 06:18:29 +0100

    fix: lint errors

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,21 +1,21 @@
-let menuBtn = document.getElementById("menu-btn")
-let sideNav = document.getElementById("my-side-nav")
-let closeBtn = document.getElementById("close-btn")
-let foodBtn = document.getElementById("food-btn")
-let aboutBtn = document.getElementById("about-btn")
+const menuBtn = document.getElementById('menu-btn')
+const sideNav = document.getElementById('my-side-nav')
+const closeBtn = document.getElementById('close-btn')
+const foodBtn = document.getElementById('food-btn')
+const aboutBtn = document.getElementById('about-btn')
 
 menuBtn.onclick = function () {
-  sideNav.style.display = "block"
+  sideNav.style.display = 'block'
 }
 
 closeBtn.onclick = function () {
-  sideNav.style.display = "none"
+  sideNav.style.display = 'none'
 }
 
 foodBtn.onclick = function () {
-  sideNav.style.display = "none"
+  sideNav.style.display = 'none'
 }
 
 aboutBtn.onclick = function () {
-  sideNav.style.display = "none"
+  sideNav.style.display = 'none'
 }
